0e4a8123408aae70bfe2ba528430d108f7486df8
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2024-03-06 12:57:23 +0900

    [UA-8] feat: add Interceptor to wrapping successful response

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 5051;
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(port, () => {
     console.log(`[CultureLand_BE] Server is running at port : ${port}`);
   });
