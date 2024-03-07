556a1c092c475ef9c739e80570dcfe235f7a9cd6
wheesunglee <wlee9486@naver.com>
2024-03-08 00:46:29 +0900

    [UA-52] feat:  add auth guard\n

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/httpException.filter';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 5051;
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
