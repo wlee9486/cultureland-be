b77ff21fec7b4dfe9cc8e9ad0af0a86c0b766551
wheesunglee <wlee9486@naver.com>
2024-03-07 17:51:04 +0900

    [UA-27] feat:   add validation pipe to app\n

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 5051;
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(port, () => {
